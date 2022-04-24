9072832335e14b70ffa853d6dde5cd8541ad3468
IronBatman2715 <72224765+IronBatman2715@users.noreply.github.com>
2022-04-23 23:08:28 -0400

    Add some try/catch blocks
    * Add try/catch block to Client.start().
    * Add try/catch block to DB.connect().
    * Add more comments about environment variables.
    * Update dependencies.

--- src/global.d.ts
+++ src/global.d.ts
@@ -1,9 +1,9 @@
 declare global {
   namespace NodeJS {
     interface ProcessEnv {
-      DISCORD_TOKEN: string; //your discord bot token
-      DB_TOKEN: string; //your mongodb database token
-      CLIENT_ID: string; //your bot's client ID
+      DISCORD_TOKEN: string; //your discord bot token (https://discord.com/developers/applications)
+      DB_TOKEN: string; //your mongodb database token (varys depending on database server)
+      CLIENT_ID: string; //your bot's client ID (https://discord.com/developers/applications)
       TEST_GUILD_ID?: string; //the guildId of your testing server
       DEV_IDS?: string; //a list of developer discord user Ids. Ex: DEV_IDS = DEV_ID1, DEV_ID2, DEV_ID3
     }
