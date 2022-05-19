3f929aa5b0030945220089bcb32d4c6877777d41
IronBatman2715 <72224765+IronBatman2715@users.noreply.github.com>
2022-05-18 20:27:35 -0400

    Refactor command registration
    * Remove promise methods from registerGlobalCommands.ts. Client.registerCommands() has sufficient handling built in.
    * Remove commented out bits.
    * Add meta data to logging before attempting to register commands.

--- src/registerGlobalCommands.ts
+++ src/registerGlobalCommands.ts
@@ -1,25 +1,8 @@
 import { config as configDotEnv } from "dotenv";
 
 import Client from "./structures/Client";
-import sleep from "./functions/general/sleep";
-import logger from "./logger";
 
 configDotEnv();
 const client: Client = new Client();
 
-let exitCode = 1;
-
-client
-  .registerCommands(true)
-  .then(() => {
-    logger.info("Successfully registered global commands!");
-    exitCode = 0;
-  })
-  .catch((error) => {
-    logger.error(error);
-    logger.error(new Error("FAILED to register global commands!"));
-  })
-  .finally(() => {
-    logger.info("Closing process in 3 seconds...");
-    sleep(3000).then(process.exit(exitCode));
-  });
+client.registerCommands(true);
