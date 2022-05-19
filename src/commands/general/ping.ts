ae8f266495fa5bbff43f0297608a3fc8d9096962
IronBatman2715 <72224765+IronBatman2715@users.noreply.github.com>
2022-05-18 22:29:58 -0400

    Refactor ping to be more verbose

--- src/commands/general/ping.ts
+++ src/commands/general/ping.ts
@@ -4,17 +4,27 @@ import type { Message } from "discord.js";
 import Command from "../../structures/Command";
 
 export = new Command(
-  new SlashCommandBuilder().setName("ping").setDescription("Shows the ping of the bot."),
+  new SlashCommandBuilder()
+    .setName("ping")
+    .setDescription("Shows the ping of the bot.")
+    .addBooleanOption((option) => option.setName("verbose").setDescription("Show more verbose description.")),
 
   async (client, interaction) => {
-    const clientPing = (await interaction.followUp({
-      content: `Ping: ${client.ws.ping} ms.`,
+    const verbose = interaction.options.getBoolean("verbose") || false;
+
+    const pingVerbose = ` Average time between regularly sent signals (websocket heartbeat).`;
+    const pingStr = `Ping: **${client.ws.ping} ms**.${verbose ? pingVerbose : ""}`;
+
+    const pingMessage = (await interaction.followUp({
+      content: pingStr,
     })) as Message<boolean>;
 
-    return await clientPing.edit({
-      content: `Ping: ${client.ws.ping} ms. \nMessage Ping: ${
-        clientPing.createdTimestamp - interaction.createdTimestamp
-      } ms.`,
+    const rtLatencyVerbose = ` Time between command message creation and response message creation.`;
+    const rtLatency = pingMessage.createdTimestamp - interaction.createdTimestamp;
+    const rtLatencyStr = `Roundtrip latency: **${rtLatency} ms**.${verbose ? rtLatencyVerbose : ""}`;
+
+    return await pingMessage.edit({
+      content: `${pingStr}\n${rtLatencyStr}`,
     });
   }
 );
