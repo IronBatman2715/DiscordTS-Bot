ab2ebb8e9e2f50dbe75edbde82e98d0ad1b975a8
IronBatman2715 <72224765+IronBatman2715@users.noreply.github.com>
2022-05-24 21:21:32 -0400

    Add more validation to Client.genEmbed()
    * Add validation steps in Client.genEmbed() to shorten strings that are too long. (Does NOT check for limit of 6000 total characters in entire message, as this only generates the embed).
    * Add optional flag to CLIENT_ID because it is only required in the development environment.

--- src/global.d.ts
+++ src/global.d.ts
@@ -8,7 +8,7 @@ declare global {
 
       //Development only variables
       /** Discord bot's client ID (found {@link https://discord.com/developers/applications here}) */
-      CLIENT_ID: string;
+      CLIENT_ID?: string;
       /** The guildId of your testing server */
       TEST_GUILD_ID?: string;
       /** A list of developer discord user Ids separated by ", ". Ex: DEV_IDS = DEV_ID1, DEV_ID2, DEV_ID3 */
