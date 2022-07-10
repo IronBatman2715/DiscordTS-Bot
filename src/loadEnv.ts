dabbe5e13ca1502f7b95c5777916cf81a43b142c
IronBatman2715 <72224765+IronBatman2715@users.noreply.github.com>
2022-07-09 20:21:36 -0400

    Refactor env loading

--- src/loadEnv.ts
+++
@@ -1,8 +0,0 @@
-import { config } from "dotenv";
-
-/** Load the appropriate .env file (`development.env` or `production.env`) */
-const loadEnv = () => {
-  config({ path: process.env.NODE_ENV === "development" ? "development.env" : "production.env" });
-};
-
-export default loadEnv;
