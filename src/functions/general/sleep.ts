a120dcb7fac266e465a43b32c15f31a537740f68
IronBatman2715 <72224765+IronBatman2715@users.noreply.github.com>
2024-01-15 14:30:51 -0500

    Add JSDoc to sleep function

--- src/functions/general/sleep.ts
+++ src/functions/general/sleep.ts
@@ -1,5 +1,9 @@
 import { isNaturalNumber } from "./math";
 
+/** Sleep the program for the specified nunber of milliseconds
+ *
+ *  ms value MUST be a natural number!
+ */
 export default (ms: number): Promise<void> => {
   if (!isNaturalNumber(ms)) throw new RangeError(`Called sleep function with an invalid time: ${ms} ms`);
 
